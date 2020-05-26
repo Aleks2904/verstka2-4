eacafd347abf5a7d53fb1eea19278f56be2be4a1
Aleks2904 <Garmatin171@yandex.ru>
2020-05-27 06:24:44 +0700

    рабочий галп

--- app/js/maska.js
+++ app/js/maska.js
@@ -1,5 +1,5 @@
 jQuery(function($){
-    $("#js-fast-reservation__masca-data").mask("99/99/9999",{placeholder:"ДД.ММ.ГГГГ"});
+    $("#js-fast-reservation__masca-data").mask("99/99/9999",{placeholder:"ДД/ММ/ГГГГ"});
     $("#js-fast-reservation__masca-day").mask("99");
     $("#js-fast-reservation__masca-parlor").mask("9",{placeholder:"0-3"});
     $("#js-write-to-us__input-mask").mask("+7 - (999) - 999 - 99 - 99",{placeholder:"+7 - (xxx) - xxx - xx - xx"});
